8fd59965fc0b6ec16fc3cfcd9a0994066351b862
Guvanch <mr.yazgulyyew@outlook.com>
2025-10-03 16:02:31 +0300

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Kurs
+{
+    public class Kisi
+    {
+        public string Ad { get; set; }
+        public int Yas { get; set; }
+
+        public Kisi(string ad, int yas)
+        {
+            Ad = ad;
+            Yas = yas;
+        }
+
+        public void Tanat()
+        {
+            Console.WriteLine($"Men: {Ad}, {Yas}");
+        }
+        public override string ToString() => $"{Ad}({Yas})";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿namespace CSharp_Kurs;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Types
+        // int , double, decimal, bool, string, char,
+        int a = 2;
+        double pi = 3.14159;
+        decimal aylyk = 3851556.52m;// finans ssitemasynda
+        bool test = false;
+        char xx = 'a';
+        string testData = "test Mysal";
+        var ulke = "TKM";
+        const double KDV = 1.0;
+        Console.WriteLine($"Rsult: {a}, Pi Sany: {pi}, Aylyk: {aylyk.ToString()}");
+        Console.WriteLine("Salam Dünya!");
+
+        // Şertler We Mysallar.
+        // Mysal Uni Baha Sistemasy. (IF)
+        int bal = 85;
+        if (bal >= 90)
+            Console.WriteLine("AA");
+        else if (bal >= 80)
+            Console.WriteLine("BA");
+        else if (bal >= 65)
+            Console.WriteLine("BB");
+        else
+            Console.WriteLine("Galdy!");
+
+        // Switch
+        int gun = 3;
+        switch (gun)
+        {
+            case 1: Console.WriteLine("Monday"); break;
+            case 2: Console.WriteLine("Tuesday"); break;
+            case 3: Console.WriteLine("Wednesday"); break;
+            default: Console.WriteLine("Unknown"); break;
+        }
+
+        // C# 8+ arrow Function
+        int x = 0;
+        string tip = x switch
+        {
+            < 0 => "Negatiw",
+            0 => "Nol",
+            > 0 => "Pozitif"
+        };
+        Console.WriteLine(tip);
+
+
+        // Loop 
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"For Loop: {i}");
+        }
+        // Whili
+        int j = 0;
+        while (j < 5)
+        {
+            Console.WriteLine($"While Loop: {j}");
+            j++;
+        }
+
+        string[] diller = { "C#,Java,Python,SQL" };
+        foreach (var dil in diller)
+            Console.WriteLine($"ForEach: {dil.ToString()}");
+
+        // Methodlary çagyr. 
+        Console.WriteLine(Dortgen(0));
+        Yazgy();
+        Yazgy("Muhammet");
+        var kisiCagyr = new Kisi("Guwanç",28);
+        kisiCagyr.Tanat();
+        Console.WriteLine(kisiCagyr);
+ 
+    }
+    // Methodlar. 
+   public static int Dortgen(int n) => n * n;
+    public static void Yazgy(string ad="Guwanç")
+    {
+        Console.WriteLine(ad);
+    }
+
+}
+
+
+
+
+
+
+
+//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
